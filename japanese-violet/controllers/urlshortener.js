1f95ce5e1de1310ad82da92d236ff05b003fce4b
Aron Collados <archer193@hotmail.com>
2015-10-21 23:14:42 +0200

    Depencencias actualizadas, el puto gradle, ejemplos de rutas, ejemplos de querys con cassandra

--- japanese-violet/controllers/urlshortener.js
+++ japanese-violet/controllers/urlshortener.js
@@ -7,16 +7,15 @@ module.exports = function(app){
     //URL
 
     //Pedir Url
-    app.get('/api/v1/URL/', function(req, res){
-      shortdb.FetchURI();
+    app.get('/api/v1/URL/:shorturi', function(req, res){
+      shortdb.FetchURI(shorturi);
       //res.redirect(search[0].target_);
       res.sendStatus(404);
     }),
 
     //Crear URL
-    app.post('/api/v1/URL/', function(req, res){
-        shortdb.FetchURI();
-        res.sendStatus(404);
+    app.post('/api/v1/URL/:uri', function(req, res){
+        shortdb.FetchURI(uri);
     })
 
 }
--- japanese-violet/models/shortUrlDB.js
+++ japanese-violet/models/shortUrlDB.js
@@ -5,8 +5,19 @@ var async = require('async');
 var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'urlshortener'});
 
 
+exports.CreateTable = function(callback){
+  cassandra.execute("CREATE TABLE tablename(
+   column1 name datatype PRIMARYKEY,
+   column2 name data type,
+   column3 name data type.
+   ");
+}
+
 exports.NewURI = function(callback) {
-  //cassandra.execute("Select")
+  var query = "INSERT INTO <tablename>
+(<column1 name>, <column2 name>....)
+VALUES (<value1>, <value2>....)"
+  cassandra.execute(query)
 }
 
 exports.FetchURI = function(callback,shortUri) {
