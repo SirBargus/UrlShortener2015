c0e2eeb0ae83355a755baa36c346a83dfc641adb
Aron Collados <archer193@hotmail.com>
2015-10-21 20:15:04 +0200

    Estructura Inicial proyecto, Rutas API, librerias cassandra

--- japanese-violet/README.md
+++ japanese-violet/README.md
@@ -1,2 +1,6 @@
 # URLshortener
 URLshortener by Web Engineering written in Node.js @Unizar
+
+
+Tecnologias
+Cassandra 2.1.11
--- japanese-violet/models/shortUrlDB.js
+++ japanese-violet/models/shortUrlDB.js
@@ -1,48 +1,14 @@
-//shortUrlDB.js
 
-//shortUrl Database
+var cassandra = require('cassandra-driver');
+var async = require('async');
 
-var alasql = require('alasql');
+var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'urlshortener'});
 
-var db = new alasql.Database();
 
-db.exec("CREATE TABLE shorturl (hash STRING, target_ STRING, sponsor STRING," 
-    + "created DATE, owner STRING, mode INT, safe BOOLEAN, ip STRING," 
-    + "country STRING)");
+exports.NewURI = function(callback) {
+  //cassandra.execute("Select")
+}
 
-module.exports = {
-    add: function(su){
-        //Create md5 hash from string
-        db.exec("INSERT INTO shorturl VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", [su.hash,
-                su.target, su.sponsor, su.created, su.owner, su.momde, su.safe,
-                su.ip, su.country]);
-        return su.hash;
-    },
-    findByHash: function(hash){
-        return db.exec("select * from shorturl where hash=?",[hash]);
-    },
-    update: function(su){
-        db.exec("update shorturl set target_=?, sponsor=?, created=?, owner=?," 
-                + "mode=?, safe=?, ip=?, country=? where hash=?", [su.target, 
-                su.sponsor, su.created, su.owner, su.momde, su.safe,
-                su.ip, su.countryi, su.hash]);
-    },
-    delete_: function(hash){
-        db.exec("delete from shorturl where hash=?", [hash]);
-    },
-    deleteAll: function(){
-        db.exec("delete from shorturl");
-    },
-    count: function(){
-        return db.exec("select count(*) from shorturl");
-    },
-    list: function(limit, offset){
-        return db.exec("select * from shorturl limit ? offset ?");
-    },
-    clicksByHash: function(target){
-        return db.exec("select count(*) from shorturl where target_=?", [target]);
-    },
-    mark: function(su, safeness){
-        db.exec("update shorturl set safe=? where hash=?", [safeness, su.hash]);
-    }
+exports.FetchURI = function(callback,shortUri) {
+  cassandra.execute("Select URI from Urls WHERE %s"(shortUri))
 }
