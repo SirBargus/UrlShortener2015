cf733c904606f38d51b2a1c47f1836d09c7869f5
Agustin <agusnavarro11@gmail.com>
2015-11-16 20:05:58 +0100

    Notable finish

--- japanese-violet/controllers/qr.js
+++ japanese-violet/controllers/qr.js
@@ -2,7 +2,10 @@
 var ddbbUri = require('../models/shortUrlDB.js'),
     conf = require('../config/conf'),
     crypto = require('crypto'),
-    http = require('http');
+    http = require('http'),
+    vCard = require('vcards-js'),
+    urlencode = require('urlencode'),
+    fs = require('fs');
 
 // File with only server's methods
 
@@ -12,6 +15,7 @@ module.exports = function(app){
     app.put(conf.api.qr, function(req, res){
         if (conf.log == true) console.log("Input Conex: " + req);
         if (req.body.urlsource == undefined) {
+            if (conf.log == true) console.error("Miss urlsource in json");
             res.sendStatus(400);
             return 0;
         }
@@ -19,25 +23,72 @@ module.exports = function(app){
         var json = {"urlShort": urlShort, "urlSource": req.body.urlsource};
         var urlShortComplete = "http://" + conf.ip + ":" + conf.port + "/" + urlShort;
 
-        //Get QR image
-        http.get(conf.extern.qr + urlShortComplete, function(response){
-           if (response.statusCode == 200){
-               var img = '';
-               response.setEncoding('binary');
-               //Get the image
-               response.on('data', function(chunk){
-                   img += chunk;
-               });
-               //Finish img
-               response.on('end', function(){
-                   json.qr = img;
-                   ddbbUri.add(json);
-                   res.send(json);
-               });
-           } else res.sendStatus(400);
+        //Level of error
+        if (req.body.err == true) ext = conf.exter.qrErr + req.body.errLevel + "&chl=";
+        else ext = conf.extern.qr;
 
-        }).on('error', function(e){
-            if (conf.log == true) console.error("Got error: " + e.message);
-        });
+        //Get QR imagec
+        getQr(ext + urlShortComplete, json, res);
+    }),
+    //Create a qr with a VCARD
+    app.put(conf.api.vcard, function(req, res){
+        if (conf.log == true) console.log("Input Conex: " + req);
+        //Exist a urlsource
+        if (req.body.urlsource == undefined) {
+            if (conf.log == true) console.error("Miss urlsource in json");
+            res.sendStatus(400);
+            return 0;
+        }
+        var urlShort = crypto.createHash('md5').update(req.body.urlsource).digest('hex');
+        var json = {"urlShort": urlShort, "urlSource": req.body.urlsource};
+        var urlShortComplete = "http://" + conf.ip + ":" + conf.port + "/" + urlShort;
+
+        //Level of error
+        if (req.body.err == true) ext = conf.exter.qrErr + req.body.errLevel + "&chl=";
+        else ext = conf.extern.qr;
+
+        //Create vCard
+        vcard = vCard();
+        //Json from VCard
+        if (req.body.firstName != undefined) vcard.firstName = req.body.firstName;
+        if (req.body.middleName != undefined) vcard.middleName = req.body.middleName;
+        if (req.body.lastName != undefined) vcard.lastName = req.body.lastName;
+        if (req.body.organization != undefined) vcard.organization = req.body.organization;
+        if (req.body.photo != undefined) vcard.photo.attachFromUrl(req.body.photo);
+        if (req.body.workPhone != undefined) vcard.workPhone = req.body.workPhone;
+        if (req.body.birthday != undefined) vcard.birthday = req.body.birthday;
+        if (req.body.title != undefined) vcard.title = req.body.title;
+        vcard.url = urlShortComplete;
+
+
+        //Create qR
+        getQr(ext + urlencode(vcard.getFormattedString()), json, res);
     })
+}
+
+//Get qr from google service
+function getQr(url, json, res){
+    http.get(url, function(response){
+       if (response.statusCode == 200){
+           var img = '';
+           response.setEncoding('binary');
+           //Get the image
+           response.on('data', function(chunk){
+               img += chunk;
+           });
+           //Finish img
+           response.on('end', function(){
+               json.qr = img;
+               ddbbUri.add(json);
+               res.send(json);
+           });
+       } else {
+           if (conf.log == true) console.error("Error get to google api for qr");
+           res.sendStatus(400);
+       }
+
+    }).on('error', function(e){
+        if (conf.log == true) console.error("Got error: " + e.message);
+        res.sendStatus(400);
+    });
 }
--- japanese-violet/test/qr.js
+++ japanese-violet/test/qr.js
@@ -7,22 +7,25 @@ var conf = require('../config/conf'),
     should = require('should'),
     qrreader = require('qrcode-reader'),
     fs = require('fs'),
-    png = require('png-js');
+    png = require('png-js'),
+    vcardparse = require('vcard');
 
-var urlTest = "http://google.es";
-var urlShort = "http://" + conf.ip + ":" + conf.port + "/" 
+var urlTest = "http://www.nyan.cat";
+var urlShort = "http://" + conf.ip + ":" + conf.port + "/"
     + crypto.createHash('md5').update(urlTest).digest('hex');
 var urlIDShort = crypto.createHash('md5').update(urlTest).digest('hex');
 var json = '';
 var qrUrl = '';
+var jsonVcard = {"firstName": "dummy","middleName": "dummy","lastName": "dummy",
+    "organization": "dummy","photo": "https://pbs.twimg.com/profile_images/1620149654/avatar.jpg",
+    "workPhone": "123","birthday":new Date("14-03-1994"),"title":"dummy","urlsource":urlTest};
 //Base test
 describe('#QR test', function(){
     //Get QR
     it('Get Qr', function(done){
-        this.timeout(30000);
         request(app)
             .put(conf.api.qr)
-            .send({"urlsource": urlTest})
+            .send({"urlsource": urlTest, "qrErr": true })
             .expect(200)
             .end(function(err, res){
                 json = res.body;
@@ -30,7 +33,7 @@ describe('#QR test', function(){
             });
     }),
     it('Qr is correct', function(done){
-        //We need save qr, qr-reader modules are a shit
+        //We need save qr, qr-reader modules are shit
         fs.writeFileSync('test.png', json.qr, 'binary');
         c = fs.readFileSync('test.png');
         var p = new png(c);
@@ -62,6 +65,52 @@ describe('#QR test', function(){
             if(err) console.error("Error delete: " + err);
             else done();
         });
+    }),
+    it('Get Qr with error_correction_level', function(done){
+        request(app)
+            .put(conf.api.qr)
+            .send({"urlsource": urlTest, "qrErr": true, "errLevel": "H" })
+            .expect(200)
+            .end(function(err, res){
+                json = res.body;
+                done();
+            });
+    }),
+    it('Qr with errors is correct', function(done){
+        //We need save qr, qr-reader modules are a shit
+        var c = fs.readFileSync('./test/qr-error.png');
+        var p = new png(c);
+        p.decode(function(data){
+            var qrr = new qrreader();
+            qrr.callback = function(result){
+                //url short redirect to urlsource, test in base.js
+                result.should.equal(urlShort);
+                done();
+            }
+            qrr.decode(p, data);
+        });
+    }),
+    //Delete database's info
+    after(function(done){
+        ddbb.remove(urlIDShort, function(err){
+            if (err) console.error("Error delete: " + err);
+            else done();
+        });
+    }),
+    it('Get Qr with vcard', function(done){
+        request(app)
+            .put(conf.api.vcard)
+            .send(jsonVcard)
+            .expect(200)
+            .end(function(err, res){
+                json = res.body;
+                done();
+            });
+    }),
+    after(function(done){
+        ddbb.remove(urlIDShort, function(err){
+            if (err) console.error("Error delete: " + err);
+            else done();
+        });
     })
 });
-
